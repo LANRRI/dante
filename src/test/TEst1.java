53710cd660c50bceedda39a1ff85a7742052c26a
lanrri <sa615316@mail.ustc.edu.cn>
2021-03-30 10:15:08 +0800

    add test files

---
+++ src/test/TEst1.java
@@ -0,0 +1,10 @@
+package test;
+
+public class TEst1 {
+    public void quickSort(int [] arr){
+
+    }
+
+
+    public quickSort()
+}
---
+++ src/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Test {
+    public int binarySearch(int[] nums,int target){
+        int start = 0;
+        int end = nums.length-1;
+        while (start < end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] < target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    yilog(h(x)) + (1-yi)log(1-h(x))
+    h(x) = 1/(1+e^x);
+}
